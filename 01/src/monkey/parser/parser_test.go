c8e78da90758a3440ba87b9694bd336db1943554
Finn Buick <finnbuick@gmail.com>
2024-12-01 21:32:43 +0000

    parsing indentifiers and integer literals, page 58

--- 01/src/monkey/parser/parser_test.go
+++ 01/src/monkey/parser/parser_test.go
@@ -117,7 +117,7 @@ func TestIdentifierExpression(t *testing.T) {
 	checkParseErrors(t, p)
 
 	if len(program.Statements) != 1 {
-		t.Fatalf("program has no enough statements. got %d", len(program.Statements))
+		t.Fatalf("program has no enough statements. got=%d", len(program.Statements))
 
 	}
 	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
@@ -130,5 +130,30 @@ func TestIdentifierExpression(t *testing.T) {
 	}
 	if ident.TokenLiteral() != "foobar" {
 		t.Errorf("ident.TokenLiteral not %s. got=%s", "foobar", ident.TokenLiteral())
+	}
+}
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has no enough statements. got=%d", len(program.Statements))
+
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if literal.Value != 5 {
+		t.Errorf("ident.Value not %d. got=%d", 5, literal.Value)
+	}
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", "5", literal.TokenLiteral())
 	}
 }
